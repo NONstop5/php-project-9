3e7c9baa778a87c296458a54ff3d38e5eb8d046c
IgorIlyaguev <non_2002@mail.ru>
2024-10-14 20:59:11 +0300

    add Routes.php

---
+++ src/Routes.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\Controller\IndexController;
+use App\Controller\UrlController;
+use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
+
+class Routes
+{
+    public static function init(App $app): void
+    {
+        $app->get('/', [IndexController::class, 'indexAction'])->setName('index');
+
+        $app->group('/urls', function (RouteCollectorProxy $group) {
+            $group->get('', [UrlController::class, 'indexAction'])->setName('urls');
+
+            $group->get('/{id}', [UrlController::class, 'getUrlAction'])->setName('get_url');
+        });
+    }
+}
