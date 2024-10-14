429f01bc8faad0be72d2580853546a25b5f69c36
IgorIlyaguev <non_2002@mail.ru>
2024-10-14 20:55:12 +0300

    add migrations.sql

---
+++ migrations.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS urls
+(
+    id         BIGSERIAL PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP(0) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS url_checks
+(
+    id          BIGSERIAL PRIMARY KEY,
+    url_id      INTEGER      NOT NULL,
+    status_code INTEGER      NULL,
+    h1          VARCHAR(255) NULL,
+    title       TEXT         NULL,
+    description TEXT         NULL,
+    created_at  TIMESTAMP(0) NOT NULL,
+    FOREIGN KEY (url_id) REFERENCES urls (Id) ON DELETE CASCADE
+);
