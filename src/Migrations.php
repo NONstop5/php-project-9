3ba35507f1a688a2341b7c346428993136505de7
IgorIlyaguev <non_2002@mail.ru>
2024-10-16 16:20:14 +0300

    add Migrations.php

---
+++ src/Migrations.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Exception;
+use PDO;
+
+class Migrations
+{
+    /**
+     * @throws Exception
+     */
+    public function run(PDO $conn): void
+    {
+        $migrationsFilePath = dirname(__DIR__) . '/data/migrations.sql';
+        $sql = file_get_contents($migrationsFilePath);
+
+        if ($sql === false) {
+            throw new Exception('Unable to read migrations file!');
+        }
+
+        $conn->exec($sql);
+    }
+
+}
