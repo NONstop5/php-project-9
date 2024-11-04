fc02474b1f6edbdbb8beb3ba7c289c3f4670ea8c
IgorIlyaguev <non_2002@mail.ru>
2024-11-04 22:50:47 +0300

    fix UrlRepository.php to create and get list of urls

--- src/Repository/UrlRepository.php
+++ src/Repository/UrlRepository.php
@@ -4,6 +4,33 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use PDO;
+
 class UrlRepository
 {
+    private PDO $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function getList(): array
+    {
+        $sql = "SELECT * FROM urls";
+
+        $stmt = $this->pdo->query($sql);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function insert(string $name): void
+    {
+        $sql = "INSERT INTO urls (name) VALUES (:name)";
+
+        $this->pdo
+            ->prepare($sql)
+            ->execute(compact('name'))
+        ;
+    }
 }
