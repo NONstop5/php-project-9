26ba1d757855eec53b50cd9c53dda17b8952644d
IgorIlyaguev <non_2002@mail.ru>
2024-11-04 22:52:18 +0300

    fix UrlCheckRepository.php to add constructor

--- src/Repository/UrlCheckRepository.php
+++ src/Repository/UrlCheckRepository.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use PDO;
+
 class UrlCheckRepository
 {
+    private PDO $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
 }
