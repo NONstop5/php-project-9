aaef0f35c3d4b48354ff83b788b25c3fab379e0e
IgorIlyaguev <non_2002@mail.ru>
2024-11-06 16:51:17 +0300

    fix UrlCheckRepository.php to get ursl chacks data

--- src/Repository/UrlCheckRepository.php
+++ src/Repository/UrlCheckRepository.php
@@ -14,4 +14,21 @@ class UrlCheckRepository
     {
         $this->pdo = $pdo;
     }
+
+    public function getUrlChecks(int $id): array
+    {
+        $sql = "
+            select
+                uc.*
+            from urls u
+            left join url_checks uc on u.id = uc.url_id
+            where u.id = :id
+            order by u.id desc
+        ";
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(compact('id'));
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
