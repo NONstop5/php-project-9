b05e3fc6d9bf5ece49514ba89b7576f2e498fec3
IgorIlyaguev <non_2002@mail.ru>
2024-11-18 06:56:35 +0300

    fix index.php to add custom error handler and refactor

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Connection;
+use App\Handlers\HttpErrorHandler;
 use App\Migrations;
 use App\Routes;
 use DI\Container;
@@ -26,14 +27,29 @@ session_start();
 
 $container = new Container();
 
-$container->set(PDO::class, fn () => $conn);
-$container->set('view', fn () => new PhpRenderer(__DIR__ . '/../templates/php-view'));
-$container->set('flash', fn () => new Messages());
+$container->set(PDO::class, fn() => $conn);
+$container->set(PhpRenderer::class, fn() => new PhpRenderer(__DIR__ . '/../templates/php-view'));
+$container->set(Messages::class, fn() => new Messages());
 
 $app = AppFactory::createFromContainer($container);
 
+$callableResolver = $app->getCallableResolver();
+$responseFactory = $app->getResponseFactory();
+
+// Определяем пользовательский обработчик ошибок
+$httpErrorHandler = new HttpErrorHandler(
+    $callableResolver,
+    $responseFactory,
+    $container->get(PhpRenderer::class)
+);
+
+// Добавление промежуточного ПО маршрутизации
 $app->addRoutingMiddleware();
+
+// Добавляем промежуточное ПО обработки ошибок
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware->setDefaultErrorHandler($httpErrorHandler);
+
 $app->add(MethodOverrideMiddleware::class);
 
 Routes::init($app);
