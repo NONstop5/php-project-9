25acfc2c9d346739020aa939fafe830d02498eeb
IgorIlyaguev <non_2002@mail.ru>
2024-11-20 23:19:08 +0300

    add UrlCheckController.php

---
+++ src/Controller/UrlCheckController.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Carbon\Carbon;
+use Illuminate\Support\Arr;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Throwable;
+use Valitron\Validator;
+
+class UrlCheckController extends BaseController
+{
+    /**
+     * @throws Throwable
+     */
+    public function checkUrlAction(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        array $args
+    ): ResponseInterface {
+        $urlId = (int)$request->getAttribute('id');
+
+        $urlData = $this->urlRepository->getUrlById($urlId);
+        $this->urlCheckRepository->create($urlData['id'], ['createdAt' => Carbon::now()]);
+
+        return $response
+            ->withHeader('Location', sprintf('/urls/%s', $urlData['id']))
+            ->withStatus(302);
+    }
+}
