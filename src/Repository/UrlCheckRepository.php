5001e02700ab46348a71f762b3b64328d24b862f
IgorIlyaguev <non_2002@mail.ru>
2024-11-29 07:36:12 +0300

    fix UrlCheckRepository.php to add method

--- src/Repository/UrlCheckRepository.php
+++ src/Repository/UrlCheckRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use Carbon\Carbon;
+use InvalidArgumentException;
 use PDO;
 
 class UrlCheckRepository
@@ -29,6 +30,27 @@ class UrlCheckRepository
 
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute(compact('id'));
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getUrlsLastChecks(): array
+    {
+        $sql = "
+            SELECT
+                DISTINCT url_id,
+                MAX(created_at) as url_check_date,
+                status_code as url_check_status_code
+            FROM url_checks
+            GROUP BY url_id, status_code
+            ORDER BY url_id DESC, MAX(created_at) DESC
+        ";
+
+        $stmt = $this->pdo->query($sql);
+
+        if ($stmt === false) {
+            throw new InvalidArgumentException();
+        }
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
