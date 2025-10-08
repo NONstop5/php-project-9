3e225b180f15c1e0f2ae31791161b0efc340cbe1
IgorIlyaguev <non_2002@mail.ru>
2025-10-08 11:10:42 +0300

    refactor Routes.php

--- src/Routes.php
+++ src/Routes.php
@@ -22,11 +22,8 @@ class Routes
 
         $app->group('/urls', function (RouteCollectorProxy $group) {
             $group->get('', [UrlController::class, 'indexAction'])->setName('urls.index');
-
             $group->post('', [UrlController::class, 'createAction'])->setName('urls.create');
-
             $group->get('/{id:[0-9]+}', [UrlController::class, 'getUrlAction'])->setName('urls.show');
-
             $group->post('/{id:[0-9]+}/checks', [UrlCheckController::class, 'checkUrlAction'])->setName('urls.check');
         });
     }
